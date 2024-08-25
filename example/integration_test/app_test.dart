c368449e8f6f866cba4a267db544d0d1c3470591
Joscha <34318751+josxha@users.noreply.github.com>
2024-08-25 10:50:27 +0200

    add basic test setup

---
+++ example/integration_test/app_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'app.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('integration tests', () {
+    testWidgets('show map',
+            (tester) async {
+          await tester.pumpWidget(const App());
+          await tester.pumpAndSettle();
+        });
+  });
+}
