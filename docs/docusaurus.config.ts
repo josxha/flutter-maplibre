117fabc921051e27a827015a7447e36067bb273c
Joscha <34318751+josxha@users.noreply.github.com>
2024-09-12 12:05:03 +0200

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
     favicon: 'img/favicon-32x32.png',
 
     // Set the production url of your site here
-    url: 'https://josxha.github.io',
+    url: 'https://flutter-maplibre.pages.dev',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/flutter-maplibre/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
