beef12088407e0899e7bfb54b8028f7d48cd508c
Joscha <34318751+josxha@users.noreply.github.com>
2024-09-14 03:03:23 +0200

    more tests

--- example/integration_test/smoke_test.dart
+++ example/integration_test/smoke_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:maplibre/maplibre.dart';
@@ -17,10 +18,13 @@ void main() {
       },
     );
     testWidgets(
-      'jumpTo',
+      'getCamera',
       (tester) async {
         late final MapController ctrl;
-        final app = App(onMapCreated: (controller) => ctrl = controller);
+        final app = App(
+          onMapCreated: (controller) => ctrl = controller,
+          options: MapOptions(center: Position(1, 2)),
+        );
         await tester.pumpWidget(app);
         await tester.pumpAndSettle();
         await ctrl.jumpTo(center: Position(1, 1), bearing: 1, zoom: 1, tilt: 1);
@@ -34,6 +38,23 @@ void main() {
       },
     );
     testWidgets(
+      'jumpTo',
+      (tester) async {
+        late final MapController ctrl;
+        final app = App(onMapCreated: (controller) => ctrl = controller);
+        await tester.pumpWidget(app);
+        await tester.pumpAndSettle();
+        await ctrl.jumpTo(center: Position(1, 2), bearing: 1, zoom: 1, tilt: 1);
+        await tester.pumpAndSettle();
+        final camera = await ctrl.getCamera();
+        expect(camera.center.lng, closeTo(1, 0.00001));
+        expect(camera.center.lat, closeTo(2, 0.00001));
+        expect(camera.zoom, closeTo(1, 0.00001));
+        expect(camera.bearing, closeTo(1, 0.00001));
+        expect(camera.tilt, closeTo(1, 0.00001));
+      },
+    );
+    testWidgets(
       'flyTo',
       (tester) async {
         late final MapController ctrl;
@@ -41,7 +62,7 @@ void main() {
         await tester.pumpWidget(app);
         await tester.pumpAndSettle();
         await ctrl.flyTo(
-          center: Position(2, 2),
+          center: Position(2, 1),
           bearing: 2,
           zoom: 2,
           tilt: 2,
@@ -51,10 +72,30 @@ void main() {
         await tester.pumpAndSettle();
         final camera = await ctrl.getCamera();
         expect(camera.center.lng, closeTo(2, 0.00001));
-        expect(camera.center.lat, closeTo(2, 0.00001));
+        expect(camera.center.lat, closeTo(1, 0.00001));
         expect(camera.zoom, closeTo(2, 0.00001));
         expect(camera.bearing, closeTo(2, 0.00001));
         expect(camera.tilt, closeTo(2, 0.00001));
+      },
+    );
+    testWidgets(
+      'flyTo cancel',
+      (tester) async {
+        late final MapController ctrl;
+        final app = App(onMapCreated: (controller) => ctrl = controller);
+        await tester.pumpWidget(app);
+        await tester.pumpAndSettle();
+        await expectLater(
+          ctrl.flyTo(
+            center: Position(2, 2),
+            bearing: 2,
+            zoom: 2,
+            tilt: 2,
+            webSpeed: 0.1,
+            nativeDuration: const Duration(seconds: 10),
+          ),
+          throwsA(isA<PlatformException>()),
+        );
       },
     );
   });
