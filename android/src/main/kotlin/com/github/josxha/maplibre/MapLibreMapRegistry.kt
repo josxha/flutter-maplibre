94fcd62eda5996506e09981b8f9c1f1bef39010e
Joscha <34318751+josxha@users.noreply.github.com>
2024-09-30 15:24:55 +0200

    more clean up

--- android/src/main/kotlin/com/github/josxha/maplibre/MapLibreMapRegistry.kt
+++ android/src/main/kotlin/com/github/josxha/maplibre/MapLibreMapRegistry.kt
@@ -1,15 +1,14 @@
 package com.github.josxha.maplibre
 
-import android.app.Activity
 import org.maplibre.android.maps.MapLibreMap
 
 class MapLibreMapRegistry {
     companion object {
-        public val registry = HashMap<Int, MapLibreMap>()
+        private val registry = HashMap<Int, MapLibreMap>()
 
-        public fun get(viewId: Int): MapLibreMap? = registry[viewId]
+        fun get(viewId: Int): MapLibreMap? = registry[viewId]
 
-        public fun add(viewId: Int, map: MapLibreMap) {
+        fun add(viewId: Int, map: MapLibreMap) {
             registry[viewId] = map
         }
     }
