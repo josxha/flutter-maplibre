391448df91108abadd9136ff122890f74bef8ecf
Joscha <34318751+josxha@users.noreply.github.com>
2024-10-02 21:51:36 +0200

    first prototype

--- example/lib/annotations_marker_page.dart
+++ example/lib/annotations_marker_page.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/rendering.dart';
 import 'package:maplibre/maplibre.dart';
-import 'package:maplibre_example/layers_symbol_page.dart';
 
 @immutable
 class AnnotationsMarkerPage extends StatefulWidget {
@@ -24,40 +25,70 @@ class _AnnotationsMarkerPageState extends State<AnnotationsMarkerPage> {
     textField: 'Marker',
     textAllowOverlap: true,
     iconImage: 'marker',
-    iconSize: 0.08,
+    iconSize: 0.5,
     iconAnchor: IconAnchor.bottom,
     textOffset: const [0, 1],
   );
 
   late final MapController _controller;
 
+  final _globalKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Marker Annotations')),
-      body: MapLibreMap(
-        options: MapOptions(zoom: 7, center: Position(9.17, 47.68)),
-        onEvent: (event) async {
-          switch (event) {
-            case MapEventMapCreated():
-              _controller = event.mapController;
-            case MapEventStyleLoaded():
-              // add marker image to map
-              final response =
-                  await http.get(Uri.parse(LayersSymbolPage.imageUrl));
-              final bytes = response.bodyBytes;
-              await _controller.addImage('marker', bytes);
-            case MapEventClick():
-              // add a new marker on click
-              setState(() {
-                _layer.list.add(Point(coordinates: event.point));
-              });
-            default:
-              // ignore all other events
-              break;
-          }
-        },
-        layers: [_layer],
+      body: Stack(
+        children: [
+          MapLibreMap(
+            options: MapOptions(zoom: 7, center: Position(9.17, 47.68)),
+            onEvent: (event) async {
+              switch (event) {
+                case MapEventMapCreated():
+                  _controller = event.mapController;
+                case MapEventStyleLoaded():
+                  // add marker image to map
+                  /*final response =
+                      await http.get(Uri.parse(LayersSymbolPage.imageUrl));
+                  final bytes = response.bodyBytes;*/
+                  final boundary = _globalKey.currentContext!
+                      .findRenderObject()! as RenderRepaintBoundary;
+                  final image = await boundary.toImage();
+                  final byteData =
+                      await image.toByteData(format: ImageByteFormat.png);
+                  final pngBytes = byteData!.buffer.asUint8List();
+                  await _controller.addImage('marker', pngBytes);
+                case MapEventClick():
+                  // add a new marker on click
+                  setState(() {
+                    _layer.list.add(Point(coordinates: event.point));
+                  });
+                default:
+                  // ignore all other events
+                  break;
+              }
+            },
+            layers: [_layer],
+          ),
+          Positioned(
+            left: -1000000000,
+            child: RepaintBoundary(
+              key: _globalKey,
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                      size: 100,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
