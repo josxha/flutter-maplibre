3056ce5bea443393a96a80ede145b898ddd129bc
Joscha <34318751+josxha@users.noreply.github.com>
2024-10-04 02:50:53 +0200

    remove widget_state_pigeon

--- lib/src/native/platform_impl.dart
+++ lib/src/native/platform_impl.dart
@@ -3,12 +3,11 @@ import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:maplibre/maplibre.dart';
 import 'package:maplibre/src/native/widget_state_jni.dart';
-import 'package:maplibre/src/native/widget_state_pigeon.dart';
 import 'package:maplibre/src/platform_interface.dart';
 
 /// An implementation of [PlatformInterface] that uses method channels.
 final class PlatformImpl extends PlatformInterface {
   @override
-  State<MapLibreMap> createWidgetState() =>
-      Platform.isAndroid ? MapLibreMapStateJni() : MapLibreMapStatePigeon();
+  // currently only Android is supported
+  State<MapLibreMap> createWidgetState() => MapLibreMapStateJni();
 }
