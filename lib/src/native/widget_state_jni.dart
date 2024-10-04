853758afd569860c138e273ac01f3c233a1ee77c
Joscha <34318751+josxha@users.noreply.github.com>
2024-10-04 02:38:51 +0200

    fix: internal JReference since v0.12.0 (#65)

--- lib/src/native/widget_state_jni.dart
+++ lib/src/native/widget_state_jni.dart
@@ -4,7 +4,6 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:jni/_internal.dart';
 import 'package:maplibre/maplibre.dart';
 import 'package:maplibre/src/jni/jni.dart' as jni;
 import 'package:maplibre/src/native/extensions.dart';
@@ -418,15 +417,13 @@ final class MapLibreMapStateJni extends State<MapLibreMap>
 
   @override
   Future<LngLatBounds> getVisibleRegion() async {
-    final projectionRef = _jniProjection.reference;
-    final boundsRef = await runOnPlatformThread<JReference>(() {
-      final projection = jni.Projection.fromReference(projectionRef);
+    final projection = _jniProjection; // necessary to use it in platform thread
+    final jniBounds = await runOnPlatformThread<jni.LatLngBounds>(() {
       final region = projection.getVisibleRegion();
       final bounds = region.latLngBounds;
       region.release();
-      return bounds.reference;
+      return bounds;
     });
-    final jniBounds = jni.LatLngBounds.fromReference(boundsRef);
     final bounds = LngLatBounds(
       longitudeWest: jniBounds.longitudeWest,
       longitudeEast: jniBounds.longitudeEast,
