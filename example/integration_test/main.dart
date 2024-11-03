d4b9a977cf51584a52cdd71ffb45a12065da177d
Joscha <34318751+josxha@users.noreply.github.com>
2024-11-03 22:22:00 +0100

    try to use unit tests

--- example/integration_test/main.dart
+++ example/integration_test/main.dart
@@ -1,10 +1,8 @@
 import 'package:integration_test/integration_test.dart';
 
 import 'controller_test.dart' as controller;
-import 'models_test.dart' as models;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   controller.main();
-  models.main();
 }
--- test/models_test.dart
+++ test/models_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:maplibre/maplibre.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   group('Model Classes', () {
     test('QueriedLayer', () {
       const o = QueriedLayer(
