28a7247bb240b4aeff26e570ede66b54470119f9
Joscha <34318751+josxha@users.noreply.github.com>
2024-12-06 14:49:07 +0100

    successfully rendered the first map

--- maplibre_ios/ios/maplibre_ios/Package.swift
+++ maplibre_ios/ios/maplibre_ios/Package.swift
@@ -21,16 +21,8 @@ let package = Package(
             dependencies: [
                 .product(name: "MapLibre", package: "maplibre-gl-native-distribution")
             ],
-            resources: [
-                // If your plugin requires a privacy manifest, for example if it uses any required
-                // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
-                // privacy impact, and then uncomment these lines. For more information, see
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+            cSettings: [
+                .headerSearchPath("include/maplibre_ios")
             ]
         )
     ]
