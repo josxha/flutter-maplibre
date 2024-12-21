968f3c9db5c365090baa6b52718817148e41962a
Joscha <34318751+josxha@users.noreply.github.com>
2024-12-22 00:41:50 +0100

    Update Helpers.kt

--- android/src/main/kotlin/com/github/josxha/maplibre/Helpers.kt
+++ android/src/main/kotlin/com/github/josxha/maplibre/Helpers.kt
@@ -6,8 +6,10 @@ import org.maplibre.android.offline.OfflineTilePyramidRegionDefinition
 
 @Keep
 object Helpers {
-    /// The genreation of OfflineTilePyramidRegionDefinition fails in
-    // version 0.13.0 of jnigen. That's why this helper function gets used.
+    /**
+     * The genreation of OfflineTilePyramidRegionDefinition fails in
+     * version 0.13.0 of jnigen. That's why this helper function gets used.
+     */
     fun createOfflineTilePyramidRegionDefinition(
         styleURL: String?,
         bounds: LatLngBounds,
