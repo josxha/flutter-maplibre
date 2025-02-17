ff220083664470fe3c269525ee928aa459bd5d9b
Joscha <34318751+josxha@users.noreply.github.com>
2025-02-17 09:54:02 +0100

    Update maplibre_ffi.dart.m

--- maplibre_ios/lib/maplibre_ffi.dart.m
+++ maplibre_ios/lib/maplibre_ffi.dart.m
@@ -145,18 +145,86 @@ _ListenerTrampoline3 _MapLibreFFi_wrapBlockingBlock_pfv6jd(
   };
 }
 
-typedef void  (^_ListenerTrampoline3)(id arg0);
-_ListenerTrampoline3 _MapLibreFFi_wrapListenerBlock_1jdvcbf(_ListenerTrampoline3 block) NS_RETURNS_RETAINED {
+typedef void  (^_ListenerTrampoline4)(id arg0);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline4 _MapLibreFFi_wrapListenerBlock_xtuoz7(_ListenerTrampoline4 block) NS_RETURNS_RETAINED {
   return ^void(id arg0) {
     objc_retainBlock(block);
-    block(objc_retain(arg0));
+    block((__bridge id)(__bridge_retained void*)(arg0));
   };
 }
 
-typedef void  (^_ListenerTrampoline4)(id arg0, id arg1, id arg2);
-_ListenerTrampoline4 _MapLibreFFi_wrapListenerBlock_1hcfngn(_ListenerTrampoline4 block) NS_RETURNS_RETAINED {
+typedef void  (^_BlockingTrampoline4)(void * waiter, id arg0);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline4 _MapLibreFFi_wrapBlockingBlock_xtuoz7(
+    _BlockingTrampoline4 block, _BlockingTrampoline4 listenerBlock,
+    void* (*newWaiter)(), void (*awaitWaiter)(void*)) NS_RETURNS_RETAINED {
+  NSThread *targetThread = [NSThread currentThread];
+  return ^void(id arg0) {
+    if ([NSThread currentThread] == targetThread) {
+      objc_retainBlock(block);
+      block(nil, (__bridge id)(__bridge_retained void*)(arg0));
+    } else {
+      void* waiter = newWaiter();
+      objc_retainBlock(listenerBlock);
+      listenerBlock(waiter, (__bridge id)(__bridge_retained void*)(arg0));
+      awaitWaiter(waiter);
+    }
+  };
+}
+
+typedef void  (^_ListenerTrampoline5)(id arg0, id arg1, id arg2);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline5 _MapLibreFFi_wrapListenerBlock_r8gdi7(_ListenerTrampoline5 block) NS_RETURNS_RETAINED {
   return ^void(id arg0, id arg1, id arg2) {
     objc_retainBlock(block);
-    block(objc_retain(arg0), objc_retain(arg1), objc_retain(arg2));
+    block((__bridge id)(__bridge_retained void*)(arg0), (__bridge id)(__bridge_retained void*)(arg1), (__bridge id)(__bridge_retained void*)(arg2));
+  };
+}
+
+typedef void  (^_BlockingTrampoline5)(void * waiter, id arg0, id arg1, id arg2);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline5 _MapLibreFFi_wrapBlockingBlock_r8gdi7(
+    _BlockingTrampoline5 block, _BlockingTrampoline5 listenerBlock,
+    void* (*newWaiter)(), void (*awaitWaiter)(void*)) NS_RETURNS_RETAINED {
+  NSThread *targetThread = [NSThread currentThread];
+  return ^void(id arg0, id arg1, id arg2) {
+    if ([NSThread currentThread] == targetThread) {
+      objc_retainBlock(block);
+      block(nil, (__bridge id)(__bridge_retained void*)(arg0), (__bridge id)(__bridge_retained void*)(arg1), (__bridge id)(__bridge_retained void*)(arg2));
+    } else {
+      void* waiter = newWaiter();
+      objc_retainBlock(listenerBlock);
+      listenerBlock(waiter, (__bridge id)(__bridge_retained void*)(arg0), (__bridge id)(__bridge_retained void*)(arg1), (__bridge id)(__bridge_retained void*)(arg2));
+      awaitWaiter(waiter);
+    }
+  };
+}
+
+typedef void  (^_ListenerTrampoline6)(void * arg0, BOOL arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline6 _MapLibreFFi_wrapListenerBlock_10lndml(_ListenerTrampoline6 block) NS_RETURNS_RETAINED {
+  return ^void(void * arg0, BOOL arg1) {
+    objc_retainBlock(block);
+    block(arg0, arg1);
+  };
+}
+
+typedef void  (^_BlockingTrampoline6)(void * waiter, void * arg0, BOOL arg1);
+__attribute__((visibility("default"))) __attribute__((used))
+_ListenerTrampoline6 _MapLibreFFi_wrapBlockingBlock_10lndml(
+    _BlockingTrampoline6 block, _BlockingTrampoline6 listenerBlock,
+    void* (*newWaiter)(), void (*awaitWaiter)(void*)) NS_RETURNS_RETAINED {
+  NSThread *targetThread = [NSThread currentThread];
+  return ^void(void * arg0, BOOL arg1) {
+    if ([NSThread currentThread] == targetThread) {
+      objc_retainBlock(block);
+      block(nil, arg0, arg1);
+    } else {
+      void* waiter = newWaiter();
+      objc_retainBlock(listenerBlock);
+      listenerBlock(waiter, arg0, arg1);
+      awaitWaiter(waiter);
+    }
   };
 }
