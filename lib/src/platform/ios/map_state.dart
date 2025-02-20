b9d0382d1a9fb33bb2c89d58008c4db145bcb2ea
Joscha <34318751+josxha@users.noreply.github.com>
2025-02-20 13:15:15 +0100

    save

--- lib/src/platform/ios/map_state.dart
+++ lib/src/platform/ios/map_state.dart
@@ -153,7 +153,6 @@ final class MapLibreMapStateIos extends MapLibreMapStateNative
 
   @override
   void onStyleLoaded() {
-    print('onStyleLoaded: ${_mapView.styleURL.absoluteString}');
     // We need to refresh the cached style for when the style reloads.
     style?.dispose();
     final styleCtrl = style = StyleControllerIos._(_mapView.style!, _hostApi);
