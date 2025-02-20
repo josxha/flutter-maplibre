febbc0cb61bbabd5f48fd92a737164c537440b80
Joscha <34318751+josxha@users.noreply.github.com>
2025-02-20 21:12:43 +0100

    format

--- maplibre_ios/example/lib/main.dart
+++ maplibre_ios/example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:maplibre_ios/maplibre_ffi.dart';
 import 'package:maplibre_ios/maplibre_ios.dart';
