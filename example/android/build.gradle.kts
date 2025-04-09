d6e13fbfc6372e16cca14eddfbd2544250e247a6
Joscha <34318751+josxha@users.noreply.github.com>
2025-04-09 21:41:30 +0200

    format gradle files

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -20,7 +20,7 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-apply plugin: "org.jlleitschuh.gradle.ktlint"
+apply plugin : "org.jlleitschuh.gradle.ktlint"
 
 ktlint {
     version = "0.43.2"
