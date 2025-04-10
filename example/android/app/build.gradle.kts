d680bd1bfbb30af19c7a7f76b79a554dc358ce1c
Joscha Eckert <34318751+josxha@users.noreply.github.com>
2025-04-10 23:23:31 +0200

    chore: update project files (#258)
    - regenerate most platform specific project files for the package and
    example project
    - use kotlin gradle DSL for package
    - fix https://github.com/josxha/flutter-maplibre/issues/257

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.github.josxha.maplibre_example"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "26.3.11579264" // flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.github.josxha.maplibre_example"
+        // minSdk 21 required by maplibre
+        minSdk = 21 // flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    dependencies {
+        // JNIGEN       implementation(project(":MapLibreAndroid"))
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- macos/maplibre.podspec
+++ macos/maplibre.podspec
@@ -5,16 +5,23 @@
 Pod::Spec.new do |s|
   s.name             = 'maplibre'
   s.version          = '0.0.1'
-  s.summary          = 'A new Flutter project.'
+  s.summary          = 'A new Flutter plugin project.'
   s.description      = <<-DESC
-A new Flutter project.
+A new Flutter plugin project.
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
 
   s.source           = { :path => '.' }
-  s.source_files     = 'Classes/**/*'
+  s.source_files = 'maplibre/Sources/maplibre/**/*'
+
+  # If your plugin requires a privacy manifest, for example if it collects user
+  # data, update the PrivacyInfo.xcprivacy file to describe your plugin's
+  # privacy impact, and then uncomment this line. For more information,
+  # see https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+  # s.resource_bundles = {'maplibre_privacy' => ['maplibre/Sources/maplibre/PrivacyInfo.xcprivacy']}
+
   s.dependency 'FlutterMacOS'
 
   s.platform = :osx, '10.11'
