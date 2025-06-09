94085a54e4d740c7342ac394c61f3d60179cf623
Joscha <34318751+josxha@users.noreply.github.com>
2025-06-09 21:36:29 +0200

    clean up code

--- maplibre_ios/example/lib/main.dart
+++ maplibre_ios/example/lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:maplibre_ios/maplibre_ffi.dart';
-import 'package:maplibre_ios/maplibre_ios.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,31 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _maplibreIosPlugin = MaplibreIos();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    try {
-      platformVersion =
-          await _maplibreIosPlugin.getPlatformVersion() ??
-          'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,7 +20,6 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(title: const Text('Plugin example app')),
         body: Column(
           children: [
-            Text('Running on: $_platformVersion\n'),
             OutlinedButton(
               onPressed: () {
                 final store = MLNOfflineStorage.getSharedOfflineStorage();
