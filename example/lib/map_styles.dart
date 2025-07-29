fb51cb8e0e6da2985182fc77e9aa72d2550966b2
Joscha <34318751+josxha@users.noreply.github.com>
2025-07-29 23:47:55 +0200

    merge style asset directories

--- example/lib/map_styles.dart
+++ example/lib/map_styles.dart
@@ -1,6 +1,3 @@
-/// **Use your own key for your project!**
-///
-/// This key will be rotated occasionally.
 abstract class MapStyles {
   static const protomapsLight =
       'https://api.protomaps.com/styles/v2/light.json?key=$_protomapsKey';
@@ -8,10 +5,14 @@ abstract class MapStyles {
       'https://api.protomaps.com/styles/v2/dark.json?key=$_protomapsKey';
   static const maptilerStreets =
       'https://api.maptiler.com/maps/streets-v2/style.json?key=$_maptilerKey';
-  static const protomapsLightPmTiles =
-      'assets/style/protomaps-light.json';
+  static const protomapsLightPmTiles = 'assets/styles/protomaps-light.json';
   static const translucentAsset = 'assets/styles/translucent_style.json';
 
+  /// **Use your own key for your project!**
+  /// This key will be rotated occasionally.
   static const _maptilerKey = 'OPCgnZ51sHETbEQ4wnkd';
+
+  /// **Use your own key for your project!**
+  /// This key will be rotated occasionally.
   static const _protomapsKey = 'a6f9aebb3965458c';
 }
