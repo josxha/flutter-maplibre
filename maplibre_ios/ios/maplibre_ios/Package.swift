501251fb46c9bb8b6b4e83891827a1e6babbdfee
Joscha Eckert <34318751+josxha@users.noreply.github.com>
2025-11-02 00:18:54 +0100

    feat: catch objc exceptions, fix: int array literal (#393)
    - Ios catch objc exceptions (parseExpression() exceptions coming from
    C++, so most won't get catched)
    - improve parseException()

--- maplibre_ios/ios/maplibre_ios/Package.swift
+++ maplibre_ios/ios/maplibre_ios/Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     // Needs to be the same version as in ../maplibre_ios.podspec
     // FOR PREBUILT LIBRARY
     .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.19.0")),
+    .package(url: "https://github.com/mattgallagher/CwlCatchException.git", .upToNextMinor(from: "2.2.0")),
   ],
   targets: [
     .target(
@@ -25,6 +26,8 @@ let package = Package(
 
         // FOR LOCAL LIBRARY
         // "MapLibre",
+
+        .product(name: "CwlCatchException", package: "CwlCatchException"),
       ],
       cSettings: [
         .headerSearchPath("include/maplibre_ios"),
