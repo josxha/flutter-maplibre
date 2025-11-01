1fd84753cfaa7dc3c1b8734dea495a7331040692
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-01 15:24:57 +0100

    2 indents

--- maplibre_ios/ios/maplibre_ios/Package.swift
+++ maplibre_ios/ios/maplibre_ios/Package.swift
@@ -4,36 +4,36 @@
 import PackageDescription
 
 let package = Package(
-    name: "maplibre_ios",
-    platforms: [
-        .iOS("12.0"),
-    ],
-    products: [
-        .library(name: "maplibre-ios", targets: ["maplibre_ios"]),
-    ],
-    dependencies: [
-        // Needs to be the same version as in ../maplibre_ios.podspec
+  name: "maplibre_ios",
+  platforms: [
+    .iOS("12.0"),
+  ],
+  products: [
+    .library(name: "maplibre-ios", targets: ["maplibre_ios"]),
+  ],
+  dependencies: [
+    // Needs to be the same version as in ../maplibre_ios.podspec
+    // FOR PREBUILT LIBRARY
+    .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.19.0")),
+  ],
+  targets: [
+    .target(
+      name: "maplibre_ios",
+      dependencies: [
         // FOR PREBUILT LIBRARY
-        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.19.0")),
-    ],
-    targets: [
-        .target(
-            name: "maplibre_ios",
-            dependencies: [
-                // FOR PREBUILT LIBRARY
-                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
+        .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
 
-                // FOR LOCAL LIBRARY
-                // "MapLibre",
-            ],
-            cSettings: [
-                .headerSearchPath("include/maplibre_ios"),
-            ]
-        ),
         // FOR LOCAL LIBRARY
-        // .binaryTarget(
-        //   name: "MapLibre",
-        //   path: "../.build/MapLibre.xcframework",
-        // ),
-    ]
+        // "MapLibre",
+      ],
+      cSettings: [
+        .headerSearchPath("include/maplibre_ios"),
+      ]
+    ),
+    // FOR LOCAL LIBRARY
+    // .binaryTarget(
+    //   name: "MapLibre",
+    //   path: "../.build/MapLibre.xcframework",
+    // ),
+  ]
 )
--- maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/MapLibreRegistry.swift
+++ maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/MapLibreRegistry.swift
@@ -33,7 +33,7 @@ import UIKit
 
 @objc public class Helpers: NSObject {
   @objc public static func addImageToStyle(
-    target: NSObject, field: String, expression: NSExpression,
+    target: NSObject, field: String, expression: NSExpression
   ) {
     do {
       target.setValue(expression, forKey: field)
@@ -43,7 +43,7 @@ import UIKit
   }
 
   @objc public static func setExpression(
-    target: NSObject, field: String, expression: NSExpression,
+    target: NSObject, field: String, expression: NSExpression
   ) {
     do {
       // https://developer.apple.com/documentation/objectivec/nsobject/1418139-setvalue
@@ -54,7 +54,7 @@ import UIKit
   }
 
   @objc public static func parseExpression(
-    propertyName: String, expression: String,
+    propertyName: String, expression: String
   ) -> NSExpression? {
     print("\(propertyName): \(expression)")
     do {
@@ -67,17 +67,17 @@ import UIKit
         // can't create an Expression if the data of a literal is an array
         let json = try JSONSerialization.jsonObject(
           with: expression.data(using: .utf8)!,
-          options: .fragmentsAllowed,
+          options: .fragmentsAllowed
         )
         // print("json: \(json)")
         if let offset = json as? [Any] {
           if offset.count == 2, offset.first is String,
-            offset.first as? String == "literal"
+             offset.first as? String == "literal"
           {
             if let vector = offset.last as? [Any] {
               if vector.count == 2 {
                 if let x = vector.first as? Double,
-                  let y = vector.last as? Double
+                   let y = vector.last as? Double
                 {
                   return NSExpression(
                     forConstantValue: NSValue(
