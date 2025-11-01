1400baaf4fbae718e83997f10588ed87ee48b21f
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-01 21:16:36 +0100

    gen swift headers

--- maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/gen_swift_headers.sh
+++ maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/gen_swift_headers.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# Use for -target the same ios version that is returned by xcrun
+# Build the MapLibre iOS SDK before running this script
+
+xcrun --sdk iphonesimulator --show-sdk-path
+
 swiftc \
     -c MapLibreRegistry.swift \
+    -c Extensions.swift \
     -module-name maplibre_ios \
     -emit-objc-header-path MapLibreRegistry.h \
+    -emit-objc-header-path Helpers.h \
     -emit-library -o libmaplibreios.dylib \
-    -target arm64-apple-ios18.1-simulator \
+    -target arm64-apple-ios18.5-simulator \
     -sdk $(xcrun --sdk iphonesimulator --show-sdk-path) \
-    -F ../../../.build/MapLibre.xcframework/ios-arm64_x86_64-simulator/
+    -F ../../../.build/MapLibre.xcframework/ios-arm64_x86_64-simulator/ \
+    -framework MapLibre
