ad96ec74502875b93b96fe4bab213a76947fc689
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-01 13:42:50 +0100

    Update Package.swift

--- maplibre_ios/ios/maplibre_ios/Package.swift
+++ maplibre_ios/ios/maplibre_ios/Package.swift
@@ -4,39 +4,36 @@
 import PackageDescription
 
 let package = Package(
-  name: "maplibre_ios",
-  platforms: [
-    .iOS("12.0")
-  ],
-  products: [
-    .library(name: "maplibre-ios", targets: ["maplibre_ios"])
-  ],
-  dependencies: [
-    // Needs to be the same version as in ../maplibre_ios.podspec
-    // FOR PREBUILT LIBRARY
-    .package(
-      url: "https://github.com/maplibre/maplibre-gl-native-distribution",
-      .upToNextMinor(from: "6.19.0"),
-    )
-  ],
-  targets: [
-    .target(
-      name: "maplibre_ios",
-      dependencies: [
+    name: "maplibre_ios",
+    platforms: [
+        .iOS("12.0"),
+    ],
+    products: [
+        .library(name: "maplibre-ios", targets: ["maplibre_ios"]),
+    ],
+    dependencies: [
+        // Needs to be the same version as in ../maplibre_ios.podspec
         // FOR PREBUILT LIBRARY
-        .product(name: "MapLibre", package: "maplibre-gl-native-distribution")
+        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.19.0")),
+    ],
+    targets: [
+        .target(
+            name: "maplibre_ios",
+            dependencies: [
+                // FOR PREBUILT LIBRARY
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
 
+                // FOR LOCAL LIBRARY
+                // "MapLibre",
+            ],
+            cSettings: [
+                .headerSearchPath("include/maplibre_ios"),
+            ]
+        ),
         // FOR LOCAL LIBRARY
-        // "MapLibre",
-      ],
-      cSettings: [
-        .headerSearchPath("include/maplibre_ios")
-      ],
-    )
-    // FOR LOCAL LIBRARY
-    // .binaryTarget(
-    //   name: "MapLibre",
-    //   path: "../.build/MapLibre.xcframework",
-    // ),
-  ],
+        // .binaryTarget(
+        //   name: "MapLibre",
+        //   path: "../.build/MapLibre.xcframework",
+        // ),
+    ]
 )
