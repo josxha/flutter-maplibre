d00e7214debc50a8d853db10b086b78994865742
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-02 22:52:11 +0100

    remove CwlCatchException

--- maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/Helpers.swift
+++ maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/Helpers.swift
@@ -1,4 +1,3 @@
-import CwlCatchException
 import Foundation
 import MapLibre
 import UIKit
@@ -11,39 +10,28 @@ import UIKit
   @objc public static func addImageToStyle(
     target: NSObject, field: String, expression: NSExpression
   ) {
-    if let exception = NSException.catchException(in: {
       target.setValue(expression, forKey: field)
-    }) {
-      print("⚠️ Couldn't set expression in Helpers.addImageToStyle(): \(exception)")
-    }
   }
 
   @objc public static func setExpression(
     target: NSObject, field: String, expression: NSExpression
   ) {
-    if let exception = NSException.catchException(in: {
       // https://developer.apple.com/documentation/objectivec/nsobject/1418139-setvalue
       target.setValue(expression, forKey: field)
-    }) {
-      print("⚠️ Couldn't set expression in Helpers.setExpression(): \(exception)")
-    }
   }
 
   @objc public static func parseExpression(
     propertyName: String, expression: String
   ) -> NSExpression? {
     print("Helpers.parseExpression(): \(propertyName): \(expression)")
-    var result: NSExpression? = nil
 
-    if let exception = NSException.catchException(in: {
       // can't create an Expression using the default method if the data is a hex string
       if propertyName.contains("color"), expression.first == "#" {
         guard let color = UIColor(hexString: expression) else {
           print("⚠️ Couldn't create UIColor from hex string: \(expression)")
-          return
+          return nil
         }
-        result = NSExpression(forConstantValue: color)
-        return
+        return NSExpression(forConstantValue: color)
       }
       if expression.starts(with: "[") {
         // can't create an Expression if the data of a literal is an array
@@ -64,28 +52,21 @@ import UIKit
                let x = vector.first as? Double,
                let y = vector.last as? Double
             {
-              result = NSExpression(
+              return NSExpression(
                 forConstantValue: NSValue(cgVector: CGVector(dx: x, dy: y))
               )
-              return
             }
 
             // Case 2: simple array literal like [x, y] or [5,5]
-            result = NSExpression(forConstantValue: offset)
-            return
+            return NSExpression(forConstantValue: offset)
           }
-          result = NSExpression(mglJSONObject: json)
-          return
+          return NSExpression(mglJSONObject: json)
         } catch {
           print("⚠️ JSON parsing error in Helpers.parseExpression(): \(error)")
-          return
+          return nil
         }
       }
       // parse as a constant value
-      result = NSExpression(forConstantValue: expression)
-    }) {
-      print("⚠️ Couldn't parse expression in Helpers.parseExpression(): \(exception)")
-    }
-    return result
+      return NSExpression(forConstantValue: expression)
   }
 }
