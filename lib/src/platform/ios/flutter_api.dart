c2999b52acdd4d42dd703ad6f36ba9b32f66df63
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-02 18:13:16 +0100

    Update flutter_api.dart

--- lib/src/platform/ios/flutter_api.dart
+++ lib/src/platform/ios/flutter_api.dart
@@ -4,7 +4,8 @@ import 'package:maplibre_ios/maplibre_ffi.dart' as ffi;
 /// Call dart from native swift code.
 void createFlutterApi() => ffi.FlutterApi.implement(
   createPlatformViewWithViewId_: (viewId) {
-    final view = ffi.FlutterPlatformView();
+    final view2 = ffi.UIView();
+    final view = ffi.FlutterPlatformView.castFrom(other);
     Registry.platformViews[viewId] = view;
     return view;
   },
