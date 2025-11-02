fed5980c782bb1577b4a8b5076c50712dcf84d78
Joscha <34318751+josxha@users.noreply.github.com>
2025-11-02 23:15:56 +0100

    Update Helpers.swift

--- maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/Helpers.swift
+++ maplibre_ios/ios/maplibre_ios/Sources/maplibre_ios/Helpers.swift
@@ -10,14 +10,14 @@ import UIKit
   @objc public static func addImageToStyle(
     target: NSObject, field: String, expression: NSExpression
   ) {
-      target.setValue(expression, forKey: field)
+    target.setValue(expression, forKey: field)
   }
 
   @objc public static func setExpression(
     target: NSObject, field: String, expression: NSExpression
   ) {
-      // https://developer.apple.com/documentation/objectivec/nsobject/1418139-setvalue
-      target.setValue(expression, forKey: field)
+    // https://developer.apple.com/documentation/objectivec/nsobject/1418139-setvalue
+    target.setValue(expression, forKey: field)
   }
 
   @objc public static func parseExpression(
@@ -25,48 +25,39 @@ import UIKit
   ) -> NSExpression? {
     print("Helpers.parseExpression(): \(propertyName): \(expression)")
 
-      // can't create an Expression using the default method if the data is a hex string
-      if propertyName.contains("color"), expression.first == "#" {
-        guard let color = UIColor(hexString: expression) else {
-          print("⚠️ Couldn't create UIColor from hex string: \(expression)")
-          return nil
-        }
-        return NSExpression(forConstantValue: color)
+    // can't create an Expression using the default method if the data is a hex string
+    if propertyName.contains("color"), expression.first == "#" {
+      guard let color = UIColor(hexString: expression) else {
+        print("⚠️ Couldn't create UIColor from hex string: \(expression)")
+        return nil
       }
-      if expression.starts(with: "[") {
-        // can't create an Expression if the data of a literal is an array
-        do {
-          let json = try JSONSerialization.jsonObject(
-            with: expression.data(using: .utf8)!,
-            options: .fragmentsAllowed
-          )
-
-          // print("json: \(json)")
-          if let offset = json as? [Any] {
-            // Case 1: ["literal", [x, y]]
-            if let keyword = offset.first as? String,
-               keyword == "literal",
-               offset.count == 2,
-               let vector = offset.last as? [Any],
-               vector.count == 2,
-               let x = vector.first as? Double,
-               let y = vector.last as? Double
-            {
-              return NSExpression(
-                forConstantValue: NSValue(cgVector: CGVector(dx: x, dy: y))
-              )
+      return NSExpression(forConstantValue: color)
+    }
+    if expression.starts(with: "[") {
+      // can't create an Expression if the data of a literal is an array
+      let json = try JSONSerialization.jsonObject(
+        with: expression.data(using: .utf8)!,
+        options: .fragmentsAllowed
+      )
+      if let offset = json as? [Any] {
+        if offset.count == 2, offset.first is String,
+           offset.first as? String == "literal"
+        {
+          if let vector = offset.last as? [Any] {
+            if vector.count == 2 {
+              if let x = vector.first as? Double,
+                 let y = vector.last as? Double
+              {
+                return NSExpression(
+                  forConstantValue: NSValue(
+                    cgVector: CGVector(dx: x, dy: y)))
+              }
             }
-
-            // Case 2: simple array literal like [x, y] or [5,5]
-            return NSExpression(forConstantValue: offset)
           }
-          return NSExpression(mglJSONObject: json)
-        } catch {
-          print("⚠️ JSON parsing error in Helpers.parseExpression(): \(error)")
-          return nil
         }
       }
-      // parse as a constant value
-      return NSExpression(forConstantValue: expression)
+    }
+    // parse as a constant value
+    return NSExpression(forConstantValue: expression)
   }
 }
