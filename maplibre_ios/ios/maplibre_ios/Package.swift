c13d9dac19f4b6e5b671583542325ae5df095edf
Joscha <34318751+josxha@users.noreply.github.com>
2025-12-06 12:48:30 +0100

    add podspec integration back in
    flutter pub get throws an error on all platforms if swift package manager is not enabled. This changes the setup steps for some users, therefore we should delay it for the next breaking changes

--- maplibre_ios/ios/maplibre_ios/Package.swift
+++ maplibre_ios/ios/maplibre_ios/Package.swift
@@ -14,26 +14,26 @@ let package = Package(
   dependencies: [
     // Needs to be the same version as in ../maplibre_ios.podspec
     // FOR PREBUILT LIBRARY
-   .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.21.0")),
+//    .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution", .upToNextMinor(from: "6.19.0")),
   ],
   targets: [
     .target(
       name: "maplibre_ios",
       dependencies: [
         // FOR PREBUILT LIBRARY
-       .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
+//        .product(name: "MapLibre", package: "maplibre-gl-native-distribution"),
 
         // FOR LOCAL LIBRARY
-//          "MapLibre",
+         "MapLibre",
       ],
       cSettings: [
         .headerSearchPath("include/maplibre_ios"),
       ]
     ),
     // FOR LOCAL LIBRARY
-//      .binaryTarget(
-//        name: "MapLibre",
-//        path: "../../../../../../../maplibre_ios/ios/.build/MapLibre.xcframework",
-//      )
+     .binaryTarget(
+       name: "MapLibre",
+       path: "../../../../../../../maplibre_ios/ios/.build/MapLibre.xcframework",
+     )
   ]
 )
