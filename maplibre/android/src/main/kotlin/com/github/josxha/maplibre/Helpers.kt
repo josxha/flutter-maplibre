51bf2e9d7a235c4850b67a1e3655a2a57b84654c
Joscha <34318751+josxha@users.noreply.github.com>
2025-12-28 10:44:15 +0100

    ktlint

--- maplibre/android/src/main/kotlin/com/github/josxha/maplibre/Helpers.kt
+++ maplibre/android/src/main/kotlin/com/github/josxha/maplibre/Helpers.kt
@@ -11,7 +11,5 @@ object Helpers {
      * Parses a raw expression string into an Expression object.
      * https://github.com/dart-lang/native/issues/2903
      */
-    public fun parseExpression(raw: String): Expression {
-        return Expression.Converter.convert(raw)
-    }
+    public fun parseExpression(raw: String): Expression = Expression.Converter.convert(raw)
 }
