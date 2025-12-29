a4fa24679c728d97ea32dad882f6ddb008b35898
BartoszStasiurka <33113199+BartoszStasiurka@users.noreply.github.com>
2025-12-29 11:00:12 +0100

    SourceLayer: example app modification

--- example/lib/style_layers_vector_page.dart
+++ example/lib/style_layers_vector_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:maplibre/maplibre.dart';
+import 'package:maplibre_example/style_layers_symbol_page.dart';
 
 @immutable
 class StyleLayersVectorPage extends StatefulWidget {
@@ -18,12 +20,20 @@ class _StyleLayersVectorPageState extends State<StyleLayersVectorPage> {
       appBar: AppBar(title: const Text('Vector Style Layer')),
       body: MapLibreMap(
         options: const MapOptions(
-          initZoom: 3,
+          initZoom: 2,
           initCenter: Geographic(lon: 9.17, lat: 47.68),
         ),
         onStyleLoaded: (style) async {
           try {
             const sourceId = 'vector_source';
+
+            final response = await http.get(
+              Uri.parse(StyleLayersSymbolPage.imageUrl),
+            );
+            final bytes = response.bodyBytes;
+
+            // add the image to the map
+            await style.addImage('marker', bytes);
 
             const vectorSource = VectorSource(
               id: sourceId,
@@ -32,18 +42,50 @@ class _StyleLayersVectorPageState extends State<StyleLayersVectorPage> {
             );
             await style.addSource(vectorSource);
 
+            const fillLayer = FillStyleLayer(
+              id: 'fill_layer',
+              sourceId: sourceId,
+              sourceLayerId: 'countries',
+              paint: {'fill-color': '#429ef5', 'fill-opacity': 0.6},
+            );
+
+            await style.addLayer(fillLayer);
+
+            const lineLayer = LineStyleLayer(
+              id: 'line_layer',
+              sourceId: sourceId,
+              sourceLayerId: 'countries',
+              paint: {'line-color': '#00F', 'line-width': 3},
+            );
+
+            await style.addLayer(lineLayer);
+
             const circleLayer = CircleStyleLayer(
               id: 'circle_layer',
               sourceId: sourceId,
+              sourceLayerId: 'centroids',
               paint: {
-                'circle-radius': 8,
-                'circle-color': '#FF6B6B',
-                'circle-opacity': 0.8,
+                'circle-radius': 18,
+                'circle-color': '#000000',
+                'circle-opacity': 0.5,
                 'circle-stroke-color': '#FFF',
                 'circle-stroke-width': 2,
               },
             );
             await style.addLayer(circleLayer);
+
+            const symbolLayer = SymbolStyleLayer(
+              id: 'images_layer',
+              sourceId: sourceId,
+              sourceLayerId: 'centroids',
+              layout: {
+                'icon-image': 'marker',
+                'icon-size': 0.18,
+                'icon-allow-overlap': true,
+                'icon-anchor': 'bottom',
+              },
+            );
+            await style.addLayer(symbolLayer);
           } on Exception catch (error, stacktrace) {
             debugPrint(error.toString());
             debugPrintStack(stackTrace: stacktrace);
