e8aec3c25e7ecb0a0638e8da73ff8c40a9bd746f
Joscha <34318751+josxha@users.noreply.github.com>
2025-12-30 01:59:38 +0100

    Update build.dart

--- maplibre_ios/hook/build.dart
+++ maplibre_ios/hook/build.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
-    final builder = CBuilder.library(
-      name: 'maplibre_ffi',
-      assetName: 'lib/maplibre_ffi.g.dart',
-      sources: ['lib/maplibre_ffi.g.dart.m'],
-    );
-    await builder.run(input: input, output: output);
+    if (Platform.isIOS) {
+      final builder = CBuilder.library(
+        name: 'maplibre_ffi',
+        assetName: 'lib/maplibre_ffi.g.dart',
+        sources: ['lib/maplibre_ffi.g.dart.m'],
+      );
+      await builder.run(input: input, output: output);
+    }
   });
 }
