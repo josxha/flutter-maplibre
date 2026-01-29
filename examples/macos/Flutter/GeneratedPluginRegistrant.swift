486ebfe43cdca354101d4824bd1966278a84e115
Joscha <34318751+josxha@users.noreply.github.com>
2026-01-30 00:40:14 +0100

    swit format

--- examples/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,13 +2,12 @@
 //  Generated file. Do not edit.
 //
 
+import flutter_inappwebview_macos
 import FlutterMacOS
 import Foundation
-
-import flutter_inappwebview_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+    InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+    UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
