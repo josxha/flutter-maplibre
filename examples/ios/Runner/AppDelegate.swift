a937824f1e6245fb14e98a5e2b27329b0cb86ae0
Joscha <34318751+josxha@users.noreply.github.com>
2026-04-08 00:54:53 +0200

    Update AppDelegate.swift

--- examples/ios/Runner/AppDelegate.swift
+++ examples/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
